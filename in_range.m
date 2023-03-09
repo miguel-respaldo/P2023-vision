3c4455674547b6e0eae69f668151a9b516f577d5
Miguel Bernal Marin <miguel.bernal.marin@tec.mx>
2023-03-09 10:44:52 -0600

    rename max and min

--- in_range.m
+++ in_range.m
@@ -1,15 +1,15 @@
-function output = in_range(input, min, max)
+function output = in_range(input, minval, maxval)
 	chn_h = input(:, :, 1);
 	chn_s = input(:, :, 2);
 	chn_v = input(:, :, 3);
 
-	chn_h( min(1) > chn_h) = 0;
-	chn_s( min(2) > chn_s) = 0;
-	chn_v( min(3) > chn_v) = 0;
+	chn_h( minval(1) > chn_h) = 0;
+	chn_s( minval(2) > chn_s) = 0;
+	chn_v( minval(3) > chn_v) = 0;
 
-	chn_h( max(1) < chn_h) = 0;
-	chn_s( max(2) < chn_s) = 0;
-	chn_v( max(3) < chn_v) = 0;
+	chn_h( maxval(1) < chn_h) = 0;
+	chn_s( maxval(2) < chn_s) = 0;
+	chn_v( maxval(3) < chn_v) = 0;
 
 	% !=, is not, ~= 
 	chn_h( chn_h ~= 0) = 1;
